1196c278ef3c5748966eea6201fa471c5ec0ea93
wang-junjian <wang-junjian@qq.com>
2018-09-25 16:56:24 +0800

    update

--- ndk/class-parameter/app/src/main/java/com/wangjunjian/class_parameter/ClassParameter.java
+++ ndk/class-parameter/app/src/main/java/com/wangjunjian/class_parameter/ClassParameter.java
@@ -4,6 +4,10 @@ import android.graphics.Rect;
 
 import java.util.ArrayList;
 
+/* Terminal Window
+cd app/src/main/
+javah -d jni -classpath /Users/wjj/Library/Android/sdk/platforms/android-21/android.jar:java com.wangjunjian.class_parameter.ClassParameter
+ */
 public class ClassParameter {
     public native String hello();
     public native Rect faceDetect();
